8dd9cada227463c1d53048108c778e8f28effd99
Lucas Colucci <bublecamp@gmail.com>
2012-10-23 11:31:40 -0200

    iniciando Column Generation.

---
+++ projeto/src/tcc/pairings/graph/networks/ColumnGenerationFlightNetwork.java
@@ -0,0 +1,14 @@
+package tcc.pairings.graph.networks;
+
+import java.util.List;
+
+import tcc.pairings.Leg;
+
+public class ColumnGenerationFlightNetwork extends FlightNetwork {
+
+	public ColumnGenerationFlightNetwork(List<Leg> legs) {
+		super(legs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
